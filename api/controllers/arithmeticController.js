bc1895737af591fac2bb80f2487bc0fcf6dc814b
testrungirl <60070680+testrungirl@users.noreply.github.com>
2020-01-23 18:00:31 +0100

    Added a conversation
    I hope this works

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -32,6 +32,7 @@ exports.calculate = function(req, res) {
 
   // Validate operands
 
+  
   if (! req.query.operand1 ||
       ! req.query.operand1.match(/^(-)?[0-9\.]+(e(-)?[0-9]+)?$/) ||
       req.query.operand1.replace(/[-0-9e]/g, '').length > 1) {
